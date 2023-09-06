b461115ee57ad9830b2febc2610b904884b2a366
Maxime <maxime.marechalmccoy@gmail.com>
2023-09-06 15:23:06 -0700

    filled helper functions

--- helper-functions.js
+++ helper-functions.js
@@ -8,4 +8,45 @@ const generateRandomString = function(length = 6) {
   return newId;
 };
 
-module.exports = { generateRandomString };
+// looks up if user already exists in our data with its email
+const findUser = function(email, database) {
+  for (const user in database) {
+    if (Object.hasOwnProperty.call(database, user)) {
+      const userObj = database[user];
+      if (userObj.email === email) {
+        return userObj;
+      }
+    }
+  }
+  return null;
+};
+// looks up if user already exists in our data
+const doesExist = function(email, database) {
+  for (const user in database) {
+    console.log("🚀 ~ file: express-server.js:168 ~ doesExist ~ user:", user);
+    if (Object.hasOwnProperty.call(database, user)) {
+      const userObj = database[user];
+      console.log("🚀 ~ file: express-server.js:171 ~ doesExist ~ userObj:", userObj);
+      if (userObj.email === email) {
+        return true;
+      }
+    }
+  }
+  return false;
+};
+
+const getUserUrls = function(userId, database) {
+  let userUrls = [];
+  for (const id in database) {
+    if (Object.hasOwnProperty.call(database, id)) {
+      const element = database[id];
+      if (element.userId === userId) {
+        userUrls.push({ id, longUrl: element.longUrl });
+      }
+    }
+  }
+  return userUrls;
+};
+
+
+module.exports = { generateRandomString, getUserUrls, findUser, doesExist };
